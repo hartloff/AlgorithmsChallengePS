a6045e23c15f1c4cd12eb91288c07de1e36e034b
jessehartloff <hartloff@buffalo.edu>
2017-03-11 00:40:31 -0500

    initial commit

---
+++ src/AlgorithmsChallengePS.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class AlgorithmsChallengePS {
+
+
+
+    // 2 points
+    static ArrayList<Integer> Q1(ArrayList<Integer> input, int k){
+        // return an ArrayList containing the k largest elements from input
+
+        return null;
+    }
+
+
+    // 3 points
+    static double Q2(ArrayList<Integer> input){
+        // return the standard deviation of the values in the input ArrayList
+
+        return 0.0;
+    }
+
+
+    // 5 points
+    static void Q3(ArrayList<Integer> input){
+        // Sort the input ArrayList by absolute value
+
+
+    }
+
+
+    // 5 points
+    static int Q4(ArrayList<Integer> input, int k){
+        // return the kth largest value in the input ArrayList ignoring duplicate values.
+        // Example: if input=[9,8,5,2,8] and k=3 the return value is 5 since the duplicate 8 is ignored
+
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
